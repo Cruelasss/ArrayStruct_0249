86408ac4e9c5c747e8953a9d50a9e6d6df70e3d1
Cruelasss <irula5198@gmail.com>
2026-04-21 09:37:10 +0700

    Menambahkan input data mahasiswa (nim, nama, alamat, umur) di fungsi main

--- struct2.cpp
+++ struct2.cpp
@@ -14,3 +14,20 @@ struct Mahasiswa
     int umur;
 };
 
+int main () {
+    Mahasiswa mhs ;
+    cout << "Masukkan NIM : ";
+    cin >> mhs.nim ;
+    cout << "Masukkan Nama : ";
+    cin >> mhs.nama ;
+    cout << "Masukkan Alamat : ";
+    cin >> mhs.alamat ;
+    cout << "Masukkan Umur : ";
+    cin >> mhs.umur ;
+
+    cout << endl;
+    cout << " \n NIM : " << mhs.nim << endl ;
+    cout << "\n Nama : " << mhs.nama << endl ; 
+    cout << "\n Alamat : " << mhs.alamat << endl ;
+    cout << "\n Umur : " << mhs.umur << endl ;
+}
