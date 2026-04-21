2cd3f2844c2a8ea6c7b4ac3576d943b59cb04f7b
Cruelasss <irula5198@gmail.com>
2026-04-21 10:01:50 +0700

    menambahkan space untuk strinng

---
+++ struct3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct DetailAlamat {
+    string desa ;
+    string kota ;
+};
+
+struct Mahasiswa
+{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main () {
+    Mahasiswa mhs ;
+    cout << "Masukkan NIM : ";
+    getline(cin, mhs.nim) ;
+    cout << "Masukkan Nama : ";
+    getline(cin, mhs.nama) ;
+    cout << "Alamat Mahasiswa : " << endl ;
+    cout << " \t Masukkan Desa : ";
+    cin >> mhs.alamat.desa ;
+    cout << "Masukkan Kota : ";
+    cin >> mhs.alamat.kota ;
+    cout << "Masukkan Umur : ";
+    cin >> mhs.umur ;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim << endl ;
+    cout << "\n Nama : " << mhs.nama << endl ; 
+    cout << "\n Alamat : " ;
+    cout << "\n Desa : " << mhs.alamat.desa << endl ;
+    cout << "\n Kota : " << mhs.alamat.kota << endl ;
+    cout << "\n Umur : " << mhs.umur << endl ;
+}
